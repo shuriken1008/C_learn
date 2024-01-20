b8d945c446c37fe0163910b96a9223e22c37e33b
rumya4712 <shuriken20228@gmail.com>
2024-01-20 15:17:21 +0900

    up

---
+++ lesson10-5.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+#include <stdio.h>
+
+
+int main(void){
+    char input[50];
+    char lower[50];
+    char upper[50];
+    
+    printf("文字を入力してください。(英字)\n");
+
+    scanf("%s", input);
+
+    int i;
+    for(i=0; i<50; i++){
+        upper[i] = toupper(input[i]);
+        lower[i] = tolower(input[i]);
+    }
+    printf("%s\n", input);
+    printf("大文字にすると%sです\n", upper);
+    printf("小文字にすると%sです\n", lower);
+}
+
